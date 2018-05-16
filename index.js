a7a19b3b89e3ec85c7ed7d69cf9c4af556b2b1cc
test test <jo@jo>
2018-05-16 15:05:05 +0200

    to pup v1.4.0

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 const puppeteer = require('puppeteer');
 const CRED = require('./creds');
 
+const sleep = async (ms) => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res();
+    }, 2000)
+  });
+}
+
 const ID = {
   login: '#email',
   pass: '#pass'
 };
 
 (async () => {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
   let login = async () => {
     // login
-      await page.goto('https://facebook.com', {waitUntil: 'networkidle'});
-      await page.waitForSelector(ID.login);
-      await page.type(CRED.user);
-      await page.type('\t');
-      await page.type(CRED.pass);
-      await page.type('\r\n');
-      console.log("login done");
-      await page.waitForNavigation();
+    await page.goto('https://facebook.com', {
+      waitUntil: 'networkidle2'
+    });
+    await page.waitForSelector(ID.login);
+    console.log(CRED.user);
+    console.log(ID.login);
+    await page.type(ID.login, CRED.user);
+
+    await page.type(ID.pass, CRED.pass);
+    await sleep(500);
+
+    await page.click("#loginbutton")
+
+    console.log("login done");
+    await page.waitForNavigation();
   }
   await login();
-  await page.screenshot({path: 'facebook.png'});
+  await page.screenshot({
+    path: 'facebook.png'
+  });
 })();
